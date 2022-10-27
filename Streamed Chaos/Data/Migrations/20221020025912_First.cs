a001258e63490044a81e79b05f2920bebfac4a85
Brandon Carroll <bkc4444@gmail.com>
2022-10-26 18:19:18 -0700

    Updated the database

---
+++ Streamed Chaos/Data/Migrations/20221020025912_First.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Streamed_Chaos.Data.Migrations
+{
+    public partial class First : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProfilePicture",
+                table: "AspNetUsers");
+
+            migrationBuilder.RenameColumn(
+                name: "Discriminator",
+                table: "AspNetUsers",
+                newName: "ImagePath");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImagePath",
+                table: "AspNetUsers",
+                newName: "Discriminator");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProfilePicture",
+                table: "AspNetUsers",
+                type: "nvarchar(75)",
+                maxLength: 75,
+                nullable: true);
+        }
+    }
+}
