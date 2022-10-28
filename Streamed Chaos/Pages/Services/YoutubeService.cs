70bcfdbef64805b4be9af8cde475406f633836d3
Jade Ferguson <jadesnider2440@gmail.com>
2022-10-28 16:34:11 -0700

    Fixed code

---
+++ Streamed Chaos/Pages/Services/YoutubeService.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using Streamed_Chaos.Models;
+
+namespace Streamed_Chaos.Pages.Services
+{
+    public class YoutubeService : IYouTubeShowsService
+    {
+        private HttpClient httpClient;
+        private IConfiguration config;
+
+        public YoutubeService(IHttpClientFactory httpClient, IConfiguration config)
+        {
+            this.httpClient = httpClient.CreateClient();
+            this.config = config;
+        }
+
+        public async Task<IEnumerable<Show>> GetShows(int numberOfShows = 25)
+        {
+            var shows = await httpClient.GetStringAsync(config["FunctionGetShowsUrl"]);
+            return JsonConvert.DeserializeObject<IEnumerable<Show>>(shows);
+        }
+
+    }
+}
