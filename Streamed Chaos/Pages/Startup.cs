c454ac815381828b197edfccfdb8a38f2e552e06
Jade Ferguson <jadesnider2440@gmail.com>
2022-10-31 14:38:46 -0700

    Fetches videos and makes into a playlist

---
+++ Streamed Chaos/Pages/Startup.cs
@@ -0,0 +1,64 @@
+﻿using Streamed_Chaos.Pages.Services;
+using Streamed_Chaos.Models;
+
+namespace Streamed_Chaos.Pages
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                options.CheckConsentNeeded = ContextBoundObject => false;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddHttpClient();
+            services.AddSingleton<IYouTubeShowsService, YoutubeService>();
+            services.AddHealthChecks();
+
+            services.AddRazorPages().AddRazorRuntimeCompilation();
+        }
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.Use((context, next) =>
+            {
+                // Prevents caching of dynamic content in Azure Front Door
+                context.Response.Headers["Cache-Control"] = "no-store";
+
+                return next();
+            });
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapHealthChecks("/health");
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
